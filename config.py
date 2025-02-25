1f57786e7b455d8a4317b1aabb9508b04a67176f
wuruimiao <dev.wuruimiao@qq.com>
2025-02-25 11:33:58 +0800

    feat: on yun

--- config.py
+++ config.py
@@ -10,20 +10,27 @@ H265 = Error(2222, "h265_fail", "h265失败")
 
 def get_workers():
     worker_config = get_conf('workers')
+    workers: dict[str, FileWorker] = {}
 
-    workers: dict[str, FileWorker] = {conf['host']: FileWorker(
-        conf["status"], conf["task"], conf['host'], conf["con"])
-        for conf in worker_config}
-    for worker in workers.values():
-        worker.set_handler(do)
+    for conf in worker_config:
+        worker_id = conf['host']
+        worker = FileWorker(conf["status"], conf["task"], worker_id, conf["con"])
+        on_yun = conf.get('on_yun', True)
+        logger.info(f'{worker_id} on_yun={on_yun}')
+        worker.set_handler(handler(on_yun))
+        workers[worker_id] = worker
     return workers
 
 
-def do(task: FileTask) -> Error:
-    task_dir, _ = task.task_dir()
-    logger.info(f'start h265 {task.id} {task_dir}')
-    output, ok = run_cmd(["./h265_video.sh", task_dir], timeout=3 * 60 * 60)
-    logger.info(f"end h265 {task.id} {task_dir} {output}")
-    if ok:
-        return OK
-    return H265
+def handler(on_yun):
+    def do(task: FileTask) -> Error:
+        task_dir, _ = task.task_dir()
+        logger.info(f'start h265 {task.id} {task_dir}')
+        output, ok = run_cmd(
+            ["./h265_video.sh", task_dir, {True: 'true', False: 'false'}[on_yun]],
+            timeout=3 * 60 * 60)
+        logger.info(f"end h265 {task.id} {task_dir} {output}")
+        if ok:
+            return OK
+        return H265
+    return do
